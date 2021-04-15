ed1f3a4dd25c1d7d5761871d84e9ad36c978d1d1
Marco Fabbri <marco.fabbri@drwolf.it>
2021-04-15 16:04:43 +0200

    :construction: WIP

--- src/main/java/it/drwolf/impaqts/wrapper/query/QueryPattern.java
+++ src/main/java/it/drwolf/impaqts/wrapper/query/QueryPattern.java
@@ -3,53 +3,57 @@ package it.drwolf.impaqts.wrapper.query;
 import java.util.ArrayList;
 
 public class QueryPattern {
-    ArrayList<QueryToken> tokPattern;
-    QueryStructure structPattern;
+	// sono i blocchetti visuali
+	ArrayList<QueryToken> tokPattern;
 
-    public QueryPattern() {
+	// filtro alto livello. Esempio: tipo di implicito
+	QueryStructure structPattern;
 
-    }
+	public QueryPattern() {
 
-    public ArrayList<QueryToken> getTokPattern() {
-        return tokPattern;
-    }
+	}
 
-    public QueryStructure getStructPattern() {
-        return structPattern;
-    }
+	public String getCql() {
+		String cql = "";
+		if (this.tokPattern != null && !this.tokPattern.isEmpty()) {
+			// caso in cui ho esattamente 2 token e il secondo è un filtro con contesto
+			if (this.tokPattern.size() == 2 && this.tokPattern.get(1).getIsFilter() && this.tokPattern.get(0).getMinRepetitions() == 1
+					&& this.tokPattern.get(0).getMaxRepetitions() == 1 && this.tokPattern.get(1).getMinRepetitions() == 1
+					&& this.tokPattern.get(1).getMaxRepetitions() == 1) {
+				cql = "(meet " + this.tokPattern.get(0).getCql() + " " + this.tokPattern.get(1).getCql();
+				cql += " -" + this.tokPattern.get(1).getFilterContextLeft() + " " + this.tokPattern.get(1).getFilterContextRight()
+						+ ")";
+			}
+			// caso normale
+			else {
+				for (QueryToken t : this.tokPattern) {
+					cql += t.getCql();
+				}
+			}
+			if (this.structPattern != null) {
+				cql = this.structPattern.getCql(cql);
+			}
+		}
+		// caso in cui faccio la query solo sulla struttura
+		else if (this.structPattern != null) {
+			cql = this.structPattern.getCql();
+		}
+		return cql;
+	}
 
-    public void setTokPattern(ArrayList<QueryToken> tokPattern) {
-        this.tokPattern = tokPattern;
-    }
+	public QueryStructure getStructPattern() {
+		return this.structPattern;
+	}
 
-    public void setStructPattern(QueryStructure structPattern) {
-        this.structPattern = structPattern;
-    }
+	public ArrayList<QueryToken> getTokPattern() {
+		return this.tokPattern;
+	}
 
-    public String getCql() {
-        String cql = "";
-        if (tokPattern != null && !tokPattern.isEmpty()) {
-            // caso in cui ho esattamente 2 token e il secondo è un filtro con contesto
-            if (tokPattern.size() == 2 && tokPattern.get(1).getIsFilter()
-                    && tokPattern.get(0).getMinRepetitions() == 1 && tokPattern.get(0).getMaxRepetitions() == 1
-                    && tokPattern.get(1).getMinRepetitions() == 1 && tokPattern.get(1).getMaxRepetitions() == 1) {
-                cql = "(meet " + tokPattern.get(0).getCql() + " " + tokPattern.get(1).getCql();
-                cql += " -" + tokPattern.get(1).getFilterContextLeft() + " " + tokPattern.get(1).getFilterContextRight() + ")";
-            }
-            // caso normale
-            else {
-                for (QueryToken t : tokPattern) {
-                    cql += t.getCql();
-                }
-            }
-            if (structPattern != null) {
-                cql = structPattern.getCql(cql);
-            }
-        }
-        // caso in cui faccio la query solo sulla struttura
-        else if (structPattern != null) {
-            cql = structPattern.getCql();
-        }
-        return cql;
-    }
+	public void setStructPattern(QueryStructure structPattern) {
+		this.structPattern = structPattern;
+	}
+
+	public void setTokPattern(ArrayList<QueryToken> tokPattern) {
+		this.tokPattern = tokPattern;
+	}
 }
